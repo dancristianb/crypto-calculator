cd8b4efbce608678a191660fcf07094f3051ac68
dancristianb <dan.cristian.bota@gmail.com>
2020-06-19 16:34:31 +0300

    build crypt-calculator components structure

---
+++ app/javascript/components/Calculate.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Calculate extends Component {
+    render() {
+        return (
+            <div>Calculate</div>
+        )
+    }
+}
+
+export default Calculate
