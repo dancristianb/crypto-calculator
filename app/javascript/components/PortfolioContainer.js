434da880593cc9d659235b5e66795e6d1f6b4c37
dancristianb <dan.cristian.bota@gmail.com>
2020-06-22 19:15:04 +0300

    revisit main container

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -90,10 +90,10 @@ class PortfolioContainer extends Component {
 
         return (
             <div className="grid">
-                <div className="bg-gray-100 p-4">
+                <div className="block bg-gray-100 p-4">
                     {searchOrCalculate}
                 </div>
-                <div className="bg-gray-800">
+                <div className="block bg-gray-800 h-screen">
                     <Portfolio portfolio={this.state.portfolio}/>
                 </div>
             </div>
